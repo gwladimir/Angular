43c854b7fe6e3a6db8e8eacfc81c132d10c6f1a9
gwladimir <wlady.wg7@gmail.com>
2023-10-27 17:21:49 -0500

    data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 interface Tarjeta{
   titulo:string;
+  subtitulo:string;
+  image:string;
 }
 
 @Component({
@@ -16,12 +18,9 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     
     this.ArregloCards = [
-      {titulo:'video1'},
-      {titulo:'video2'},
-      {titulo:'video3'},
-      {titulo:'video4'},
-      {titulo:'video5'},
-      {titulo:'video6'},
+      {titulo:'video1', subtitulo: 'Subtitulo video 1', image:"https://fileinfo.com/img/ss/xl/jpg_44-2.jpg"},
+      {titulo:'video2', subtitulo: 'Subtitulo video 2', image:"https://demos.maugelves.com/jpg-formats/kitty-baseline.jpg"},
+      {titulo:'video3', subtitulo: 'Subtitulo video 3', image:"https://tinyjpg.com/images/social/website.jpg"},
     ]
   }
   
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
-  public image: string = "https://www.lavanguardia.com/andro4all/hero/2023/04/6d8841b6-9d24-457a-95c5-0d3de1d7bf5f.png?width=768&aspect_ratio=16:9&format=nowebp";
-  public title: string = "Prueba";
-  public paragraph: string = "Lorem suscipit."
-  ngOnInit(): void {
+  public image?: string;
+  public Titulo: string = "Angular";
 
+  @Input() dataEntrante:any;
+  constructor(){
+
+  }
+  ngOnInit(): void {
+    this.image = "https://www.cleverfiles.com/howto/wp-content/uploads/2018/03/minion.jpg";
   }
 }
